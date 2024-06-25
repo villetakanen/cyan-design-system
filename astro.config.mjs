9dde921682f4638078fb1c9a6840a6ba5775ecb3
Ville Takanen <vitkukissa@gmail.com>
2024-06-25 08:04:40 +0300

    feat: hosting at vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import vercelStatic from '@astrojs/vercel/static';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static',
+  adapter: vercelStatic(),
+});
