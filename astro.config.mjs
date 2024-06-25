008f6e3c5666d6064dd4857f4a9d463c3139c630
Ville Takanen <vitkukissa@gmail.com>
2024-06-25 13:21:45 +0300

    feat: fixed deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
 import vercelStatic from '@astrojs/vercel/static';
+import { defineConfig } from 'astro/config';
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   adapter: vercelStatic(),
-  integrations: [mdx()]
+  integrations: [mdx()],
 });
