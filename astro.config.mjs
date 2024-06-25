356d8aec62ce7a57408d10d6301477e5e3916dfc
Ville Takanen <vitkukissa@gmail.com>
2024-06-25 13:20:07 +0300

    feat: popover baseline support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import vercelStatic from '@astrojs/vercel/static';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   adapter: vercelStatic(),
+  integrations: [mdx()]
 });
