5de1faa71da1c4ea713d77fd3ddb1129e9c9ee1c
Ville Takanen <vitkukissa@gmail.com>
2024-12-11 21:25:36 +0200

    fix: astro 5 conversions

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,14 +2,28 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 export const principles = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.md', base: "./src/content/principles" }),
+  loader: glob({ pattern: '**/[^_]*.md', base: './src/content/principles' }),
   schema: z.object({
     title: z.string(),
     icon: z.string(),
   }),
-})
+});
+export const css = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.md', base: './src/content/css' }),
+  schema: z.object({
+    title: z.string(),
+    icon: z.string(),
+  }),
+});
+export const layout = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.md', base: './src/content/layout' }),
+  schema: z.object({
+    title: z.string(),
+    icon: z.string(),
+  }),
+});
 export const elements = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.md[x]', base: "./src/content/elements" }),
+  loader: glob({ pattern: '**/[^_]*.md[x]', base: './src/content/elements' }),
   schema: z.object({
     title: z.string(),
     nodename: z.string(),
